c914894a4cab748b3c8ee6dcff2bb1bbfb0af79d
Leonardo Omaña <leotangram@gmail.com>
2019-10-04 15:22:02 -0500

    Fixed publication form

--- client/src/components/Posts/postItem/PostItem.js
+++ client/src/components/Posts/postItem/PostItem.js
@@ -16,48 +16,34 @@ const PostItem = ({
   showActions
 }) => {
   return (
-    <div class="post bg-white p-1 my-1">
+    <div class="">
       <div>
         <Link to={`/profile/${user}`}>
-          <img class="round-img" src={avatar} alt="" />
+          <img class="" src={avatar} alt="" />
           <h4>{name}</h4>
         </Link>
       </div>
       <div>
-        <p class="my-1">{text}</p>
-        <p class="post-date">
+        <p class="">{text}</p>
+        <p class="">
           Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
         </p>
         {showActions && (
           <Fragment>
-            <button
-              onClick={e => addLike(_id)}
-              type="button"
-              class="btn btn-light"
-            >
-              <i class="fas fa-thumbs-up" />{' '}
+            <button onClick={e => addLike(_id)} type="button" class="">
+              <i class="" />{' '}
               <span>{likes.length > 0 && <span>{comments.length}</span>}</span>
             </button>
-            <button
-              onClick={e => removeLike(_id)}
-              type="button"
-              class="btn btn-light"
-            >
-              <i class="fas fa-thumbs-down" />
+            <button onClick={e => removeLike(_id)} type="button" class="">
+              <i class="" />
             </button>
-            <Link to={`/posts/${_id}`} class="btn btn-primary">
+            <Link to={`/posts/${_id}`} class="">
               Discussion{' '}
-              {comments.length > 0 && (
-                <span class="comment-count">{comments.length}</span>
-              )}
+              {comments.length > 0 && <span class="">{comments.length}</span>}
             </Link>
             {!auth.loading && user === auth.user._id && (
-              <button
-                type="button"
-                class="btn btn-danger"
-                onClick={e => deletePost(_id)}
-              >
-                <i class="fas fa-times" />
+              <button type="button" class="" onClick={e => deletePost(_id)}>
+                <i class="" />
               </button>
             )}
           </Fragment>
